497c2bbbaf2226da5768cd4042e1195c66470693
Sidney Fox <41299765+CCSFOX@users.noreply.github.com>
2018-11-29 10:19:30 -0600

    Update start.sh

--- python3.6/start.sh
+++ python3.6/start.sh
@@ -11,5 +11,17 @@ else
     echo "There is no script $PRE_START_PATH"
 fi
 
+# Required
+DB=${DB:-}
+DB_USER=${DB_USER:-}
+DB_PWD=${DB_PWD:-}
+DB_HOST=${DB_HOST:-}
+
+# Error if variables are not assigned
+if [ "${DB}" == "" ]; then echo "Environment Var 'DB' must be defined"
+elif [ "${DB_USER}" == "" ]; then echo "Environment Var 'DB_USER' must be defined"
+elif [ "${DB_PWD}" == "" ]; then echo "Environment Var 'DB_PWD' must be defined"
+elif [ "${DB_HOST}" == "" ]; then echo "Environment Var 'DB_HOST' must be defined"
+
 # Start Supervisor, with Nginx and uWSGI
 exec /usr/bin/supervisord
